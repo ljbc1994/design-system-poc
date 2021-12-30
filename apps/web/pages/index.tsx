a80de1fa00a52f7fe2ff251b0e599f4fed81e716
Louie Colgan <ljbc94@gmail.com>
2021-12-30 14:41:58 +0000

    add all

---
+++ apps/web/pages/index.tsx
@@ -0,0 +1,11 @@
+import { COLOR_BASE_GRAY_MEDIUM } from 'design-tokens/dist/web/tokens'
+
+export default function Web() {
+  return (
+    <div>
+      <h1 style={{ color: COLOR_BASE_GRAY_MEDIUM }}>
+        Web
+      </h1>
+    </div>
+  );
+}
