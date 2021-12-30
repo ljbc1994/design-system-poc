630c895f0596a8a95c917ba0f5ea3608640e9a5e
Louie Colgan <ljbc94@gmail.com>
2021-12-30 17:49:14 +0000

    chore: add tokens api endpoint

---
+++ apps/web/pages/api/tokens.ts
@@ -0,0 +1,49 @@
+import { Octokit } from "@octokit/rest";
+import path from 'path'
+import _ from 'lodash'
+
+const PACKAGE_SRC = 'packages/design-tokens'
+
+const octokit = new Octokit({
+  auth: process.env.GIT_ACCESS,
+});
+
+export default async function handler(req, res) {
+  if (req.method === "POST") {
+    try {
+      const body = JSON.parse(req.body)
+  
+      // get file from repo
+      const file = await octokit.rest.repos.getContent({
+        owner: 'ljbc1994',
+        repo: 'design-system',
+        path: `${PACKAGE_SRC}/${body.filePath}`,
+      })
+
+      const contents = JSON.parse(Buffer.from(file.data.content, 'base64').toString('utf8'))
+      const result = _.update(contents, body.path.concat(['value']).join('.'), () => body.value)
+
+      // dispatch updated file to workflow...
+      await octokit.rest.repos.createDispatchEvent({
+        owner: "ljbc1994",
+        repo: "design-system",
+        event_type: "update-tokens",
+        client_payload: {
+          // File data
+          file: path.basename(body.filePath),
+          dir: `${PACKAGE_SRC}/${path.dirname(body.filePath)}`,
+          tokens: JSON.stringify(result),
+
+          // PR data
+          commit_title: `Design token (${body.name}) updated to ${body.value}`,
+          commit_message: `Token updated for ${body.name} in ${body.filePath}`,
+          commit_body: `Design token (${body.name}) has been updated via Figma by @ljbc1994 and needs to be reviewed`
+        },
+      });
+    } catch (ex) {
+      console.log(ex);
+    }
+
+    res.status(200).json({ name: "John Doe" });
+  }
+}
--- apps/web/pages/api/tokens.tsx
+++
@@ -1,5 +0,0 @@
-export default function handler(req, res) {
-  if (req.method === "POST") {
-    res.status(200).json({ name: "John Doe" });
-  }
-}
