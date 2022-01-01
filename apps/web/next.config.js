25381155c22e5d143bd0cb28f5d1d50494139f62
Louie Colgan <ljbc94@gmail.com>
2022-01-01 18:50:45 +0000

    chore: add transpile

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,3 +1,5 @@
-module.exports = {
+const withTM = require('next-transpile-modules')(['@ljbc1994/design-tokens']); // pass the modules you would like to see transpiled
+
+module.exports = withTM({
   reactStrictMode: true,
-};
+});
